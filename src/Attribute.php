105b6d195af04e466476a6efe9ef9ca78d26a174
Barney Hanlon <barney@shrikeh.net>
2017-10-20 11:44:40 +0100

    Use branch of phpunit so we can change exception handling

--- src/Attribute.php
+++ src/Attribute.php
@@ -10,6 +10,7 @@
 namespace PHPUnit\Framework\Constraint;
 
 use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\Constraint\Exception\InvalidArgumentException;
 use PHPUnit\Framework\ExpectationFailedException;
 
 class Attribute extends Composite
@@ -18,6 +19,54 @@ class Attribute extends Composite
      * @var string
      */
     protected $attributeName;
+
+    /**
+     * Returns the value of an attribute of a class or an object.
+     * This also works for attributes that are declared protected or private.
+     *
+     * @param string|object $classOrObject
+     * @param string        $attributeName
+     *
+     * @return mixed
+     *
+     * @throws InvalidArgumentException
+     */
+    public static function readAttribute($classOrObject, $attributeName)
+    {
+        if (!\is_string($attributeName)) {
+            throw InvalidArgumentException::type(2, 'string');
+        }
+
+        if (!\preg_match('/[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*/', $attributeName)) {
+            throw InvalidArgumentException::type(2, 'valid attribute name');
+        }
+
+        if (\is_string($classOrObject)) {
+            if (!\class_exists($classOrObject)) {
+                throw InvalidArgumentException::type(
+                    1,
+                    'class name'
+                );
+            }
+
+            return static::getStaticAttribute(
+                $classOrObject,
+                $attributeName
+            );
+        }
+
+        if (\is_object($classOrObject)) {
+            return static::getObjectAttribute(
+                $classOrObject,
+                $attributeName
+            );
+        }
+
+        throw InvalidArgumentException::type(
+            1,
+            'class name or object'
+        );
+    }
 
     /**
      * @param Constraint $constraint
@@ -51,7 +100,7 @@ class Attribute extends Composite
     public function evaluate($other, $description = '', $returnResult = false)
     {
         return parent::evaluate(
-            Assert::readAttribute(
+            static::readAttribute(
                 $other,
                 $this->attributeName
             ),
